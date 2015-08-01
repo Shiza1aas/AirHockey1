b777a502eca9fb2dd1da0d5a1b9d0c2669e0d3f6
Mohammad Shahjahan <ahmsjahan@gmail.com>
2015-08-01 11:46:52 +0530

    The table with mallets.

---
+++ app/build/intermediates/res/debug/raw/simple_vertex_shader.glsl
@@ -0,0 +1,14 @@
+attribute vec4 a_Position;   
+  		
+
+
+void main()                  
+  
+{                              
+  
+	  gl_Position = a_Position;
+  
+ 					
+	  gl_PointSize = 30.0;
+
+  }   
